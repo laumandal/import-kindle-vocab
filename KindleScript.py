980defeb1c91afc776ea0c3b6a786b4e38764794
L M <lau.mandal@gmail.com>
2017-11-29 09:31:20 +0000

    Update KindleScript.py

--- KindleScript.py
+++ KindleScript.py
@@ -1,6 +1,4 @@
-
 # coding: utf-8
-
 
 import sys
 import sqlite3
@@ -40,7 +38,6 @@ else:
     data['stem'] = [replace_dotdash(stem) for stem in data.stem]
 
 
-
     def tag_word(full_string,string_to_tag,tag1,tag2):
         return full_string.replace(string_to_tag,"{0}{1}{2}".format(tag1,string_to_tag,tag2))
 
@@ -55,7 +52,6 @@ else:
         
     data.usage = [format_usage(usage, word) for (usage, word) in zip(data.usage, data.word)]
 
-
     data['tags'] = "日本語 kindle"
     data['stem'] =  data['stem'] + ": "
 
@@ -64,13 +60,11 @@ else:
     vocabfilename = timestamp.strftime('KindleVocab_%Y%b%d_%H%M.csv')
     data[::-1].to_csv(vocabfilename,header=False,index=False,encoding='utf-8')
 
-
+    #clear db on kindle
     conn=sqlite3.connect(vocabdb)
     c = conn.cursor()
-
     c.execute("""DELETE FROM WORDS;""")
     c.execute("""DELETE FROM LOOKUPS;""")
-
     conn.commit()
     conn.close()
 
