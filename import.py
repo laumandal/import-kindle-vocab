a01d479934993c20d61c1ff9d1f3a5254b2ed974
Laurence Mandal <lau.mandal@gmail.com>
2022-01-22 20:28:13 +0000

    feat: add arguments, use Fire

--- import.py
+++ import.py
@@ -1,37 +1,37 @@
 import db
 import dict_lookup
-import re
 import format
 from pathlib import Path
 from datetime import datetime
+import fire
 
-def import_vocab():
+
+def import_vocab(
+    highlight_study_word=True, tags="日本語 kindle", csv_encoding="utf-8-sig",
+):
     """Run the import vocab process and save as a CSV."""
     # import vocab from kindle db
-    df = db.import_vocab(num_days_history_or_last='last')
+    df = db.import_vocab(num_days_history_or_last="last")
 
-    # highlight the study word
-    df['front'] = df.apply(lambda x: format.highlight_lookup_word(
-        x.usage, x.word, x.stem
-    ), axis=1)
+    if highlight_study_word is true:
+        df["front"] = df.apply(
+            lambda x: format.highlight_lookup_word(x.usage, x.word, x.stem), axis=1
+        )
 
     # get definitions from the OSX dictionary for reverse of the cards
-    df['reverse'] = df['word'].apply(dict_lookup.lookup_and_filter)
-    df['tags'] = '日本語 kindle'
+    df["reverse"] = df["word"].apply(dict_lookup.lookup_and_filter)
+    df["tags"] = tags
 
     # save file out as CSV
-    # TODO: make the columns an argument ('word','title','timestamp' should be optional)
-    df_out = df[['word','front','reverse','tags', 'title', 'lookup_timestamp']]
+    df_out = df[["word", "front", "reverse", "tags", "title", "lookup_timestamp"]]
     output_folder = Path("output/")
-    output_file = output_folder / f"kindle_vocab_export_{datetime.now():%Y%m%d_%H%M}.csv"
-    df_out.to_csv(
-        output_file,
-        header=False,
-        index=False,
-        encoding='utf-8-sig'
-        )
+    output_file = (
+        output_folder / f"kindle_vocab_export_{datetime.now():%Y%m%d_%H%M}.csv"
+    )
+    df_out.to_csv(output_file, header=False, index=False, encoding=csv_encoding)
     db.save_last_run_time_str()
-    print('all done 🖖')
+    print("all done 🖖")
+
 
 if __name__ == "__main__":
-    import_vocab()
+    fire.Fire()
